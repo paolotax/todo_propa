9b46e4dec48948d645a8cc066a8344815a9778ee
paolotax <paolo.tassinari@gmail.com>
2012-04-28 22:10:41 +0200

    todopropa deploy

--- config/recipes/todopropa.rb
+++ config/recipes/todopropa.rb
@@ -2,9 +2,10 @@ namespace :todopropa do
   
   desc "Dependencies for nokogiri and rmagick"
   task :dependencies, roles: :app do
-    run "#{sudo} apt-get install libxslt-dev libxml2-dev"
-    run "#{sudo} apt-get install imagemagick libmagickwand-dev"
-    run "#{sudo} apt-get install sqlite3 libsqlite3-dev"
+    run "#{sudo} apt-get -y update"
+    run "#{sudo} apt-get -y install libxslt-dev libxml2-dev"
+    run "#{sudo} apt-get -y install imagemagick libmagickwand-dev"
+    run "#{sudo} apt-get -y install sqlite3 libsqlite3-dev"
     run "gem install taps --nori-nordoc"
     run "gem install rmagick --nori-nordoc"
   end
