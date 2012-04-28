8a8fc4afa4371170ece8f2ea811cd3a715aa2d5e
paolotax <paolo.tassinari@gmail.com>
2012-04-28 22:35:20 +0200

    todopropa deploy

--- config/recipes/todopropa.rb
+++ config/recipes/todopropa.rb
@@ -6,8 +6,8 @@ namespace :todopropa do
     run "#{sudo} apt-get -y install libxslt-dev libxml2-dev"
     run "#{sudo} apt-get -y install imagemagick libmagickwand-dev"
     run "#{sudo} apt-get -y install sqlite3 libsqlite3-dev"
-    run "gem install taps --nori-nordoc"
-    run "gem install rmagick --nori-nordoc"
+    run "gem install taps --no-ri --no-rdoc"
+    run "gem install rmagick --no-ri --no-rdoc"
   end
   after "deploy:setup", "todopropa:dependencies"
 
