0d722e9b00de2bbea80a23cccf997f6bcfecf0a5
paolotax <paolo.tassinari@gmail.com>
2012-05-10 06:55:38 +0200

    fattura steps

--- app/controllers/fattura_steps_controller.rb
+++ app/controllers/fattura_steps_controller.rb
@@ -1,16 +1,7 @@
 class FatturaStepsController < ApplicationController
   include Wicked::Wizard
-  steps :intestazione, :righe, :vacanze, :finale 
+  steps :intestazione, :scegli_appunti, :vacanze, :finale 
 
-
-  # def create
-  #   @fattura = Fattura.create
-  #   redirect_to wizard_path(steps.first, :fattura_id => @fattura.id)
-  # end
-  
-  
-  
-  
   def show
     
     # raise params[:fattura_id].inspect
@@ -18,20 +9,19 @@ class FatturaStepsController < ApplicationController
     @fattura = current_user.fatture.find(params[:fattura_id])
     
     case step
-      when :intestazione
-        if @fattura.numero.nil?
-          @fattura.numero = @fattura.get_new_id(current_user)
-          @fattura.data   = Time.now
-        end
-      when :righe
-        @fattura.add_righe_from_cliente(@fattura.cliente)    
-      
-      when :vacanze
-        @libri = Libro.vacanze
-        @libri.all.each do |l|
-          @fattura.righe.build(libro: l, prezzo_unitario: l.prezzo_consigliato)
-        end    
-            
+    when :intestazione
+      if @fattura.numero.nil?
+        @fattura.numero = @fattura.get_new_id(current_user)
+        @fattura.data   = Time.now
+      end
+    when :scegli_appunti
+      @righe = @fattura.cliente.righe.da_fatturare 
+    
+    when :vacanze
+      @libri = Libro.vacanze
+      @libri.all.each do |l|
+        @fattura.righe.build(libro: l, prezzo_unitario: l.prezzo_consigliato)
+      end    
     end
     render_wizard
   end
@@ -39,6 +29,15 @@ class FatturaStepsController < ApplicationController
   def update
     @fattura = current_user.fatture.find(params[:fattura_id])
     @fattura.attributes = params[:fattura]
+    
+    case step
+    when :scegli_appunti
+      if params[:appunti_ids].present?
+        @righe = @fattura.cliente.righe.da_fatturare.where("righe.appunto_id in (?)", params[:appunti_ids])
+        @fattura.righe << @righe
+      end   
+    end
+    
     render_wizard @fattura
   end
 
--- app/helpers/fattura_steps_helper.rb
+++ app/helpers/fattura_steps_helper.rb
@@ -1,2 +1,13 @@
 module FatturaStepsHelper
+  
+  def nested_righe_per_appunto(righe)
+    
+    righe.group_by(&:appunto).map do |a, righe|
+      content_tag :div, :class => 'dettaglio_fattura' do
+        content_tag( :div, check_box_tag("appunti_ids[]", a.id, false, {:class => 'cb-element'}) + link_to( "Ordine del #{l a.created_at, :format => :short} - copie #{a.totale_copie} - Importo #{a.totale_importo}", appunto_path(a)).html_safe, :class => 'consegna') +
+        content_tag( :table, righe.collect{ |r| render r }.join.html_safe, :class => 'table dettagli')
+      end
+    end.join.html_safe
+  end
+  
 end
