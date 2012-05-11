b2d1acfd068be23e35a0a62db27c0fd0942097f6
paolotax <paolo.tassinari@gmail.com>
2012-05-11 08:03:09 +0200

    fattura steps buttons

--- app/helpers/fattura_steps_helper.rb
+++ app/helpers/fattura_steps_helper.rb
@@ -4,7 +4,8 @@ module FatturaStepsHelper
     
     righe.group_by(&:appunto).map do |a, righe|
       content_tag :div, :class => 'dettaglio_fattura' do
-        content_tag( :div, check_box_tag("appunti_ids[]", a.id, false, {:class => 'cb-element'}) + link_to( "Ordine del #{l a.created_at, :format => :short} - copie #{a.totale_copie} - Importo #{a.totale_importo}", appunto_path(a)).html_safe, :class => 'consegna') +
+        content_tag( :div, check_box_tag("appunti_ids[]", a.id, true, {:class => 'cb-element'}) +
+        link_to( "Appunto del #{l a.created_at, :format => :short} - copie #{a.totale_copie} - Importo #{a.totale_importo}", appunto_path(a)).html_safe, :class => 'consegna') +
         content_tag( :table, righe.collect{ |r| render r }.join.html_safe, :class => 'table dettagli')
       end
     end.join.html_safe
