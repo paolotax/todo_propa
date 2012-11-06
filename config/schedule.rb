d100d02eae1ff0c4742371528e00dbd39d62a76c
paolotax <paolo.tassinari@gmail.com>
2012-11-06 08:52:53 +0100

    no schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
-set :output, "#{path}/log/cron.log"
-
-every 5.minutes do
-  runner "Cliente.ricalcola_properties"
-end
+# set :output, "#{path}/log/cron.log"
+# 
+# every 5.minutes do
+#   runner "Cliente.ricalcola_properties"
+# end
