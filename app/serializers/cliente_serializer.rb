1ab8eb7cdd14278da5a0c0c7eab812a2fa8562ce
paolotax <paolo.tassinari@gmail.com>
2013-03-08 16:13:13 +0100

    partita iva e cf serializer

--- app/serializers/cliente_serializer.rb
+++ app/serializers/cliente_serializer.rb
@@ -2,7 +2,8 @@ class ClienteSerializer < ActiveModel::Serializer
   attributes :id, :titolo, :cliente_tipo, 
            :frazione, :comune, :provincia, :indirizzo, :cap, 
            :ragione_sociale, :nome, :cognome, :abbr, 
-           :telefono, :telefono_2, :fax, :email, :latitude, :longitude
+           :telefono, :telefono_2, :fax, :email, :latitude, :longitude,
+           :partita_iva, :codice_fiscale
 
   #has_many :appunti
 
