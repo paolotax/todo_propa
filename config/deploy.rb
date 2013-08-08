21110708aed475b2c752f5b985ad8aa0a92934fc
Paolo Tassinari <paolo.tassinari@gmail.com>
2013-08-08 11:34:50 +0200

    prova ssh

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,4 +27,23 @@ set :branch, "goose"
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
+# Capistrano is using sudo, so set it to false
+#set :use_sudo, false
+
+# Your remote server is trying to checkout an SSH protected Git repository
+#set :repository, '/var/git/repositories/xxx.git' # Remote server also holds the git repository
+#set :local_repository, 'ssh://xxx/var/git/repositories/xxx.git' # Your development machine points to the remote machine 
+
+# SSH settings, also see ~/.ssh/config
+#set :user, "jebus"
+#set :domain, 'xxx.com'
+#set :port, 666
+
+# Other settings worth checking
+#ssh_options[:forward_agent] = true
+ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa")]
+#default_run_options[:pty] = true # see http://www.mail-archive.com/capistrano@googlegroups.com/msg07323.html for details
+
+
+
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
