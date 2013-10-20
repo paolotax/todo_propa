60dc0822a87a119993ee842374b5cb181b1629a7
Paolo Tassinari <paolo.tassinari@gmail.com>
2013-10-20 12:21:50 +0200

    tmp deleted .ruby-gemset .ruby-version

--- app/serializers/cliente_short_serializer.rb
+++ app/serializers/cliente_short_serializer.rb
@@ -1,12 +1,20 @@
 class ClienteShortSerializer < ActiveModel::Serializer
   attributes :id, :appunti_in_sospeso, :appunti_da_fare
 
-  def appunti_in_sospeso
-  	object.properties[:appunti_in_sospeso] if object.properties
-  end
+  # def appunti_in_sospeso
+  #   Cliente.find(object.id).properties[:appunti_in_sospeso]
+  # end
 
-  def appunti_da_fare
-  	object.properties[:appunti_da_fare] if object.properties
-  end
+  # def appunti_da_fare
+  #   Cliente.find(object.id).properties[:appunti_da_fare]
+  # end
+  
+  # def appunti_in_sospeso
+  # 	object.properties[:appunti_in_sospeso] if object.properties
+  # end
+
+  # def appunti_da_fare
+  # 	object.properties[:appunti_da_fare] if object.properties
+  # end
 
 end
