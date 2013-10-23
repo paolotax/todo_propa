5b978e395c0112be32c6c570e8832e5c67d7a4a6
Paolo Tassinari <paolo.tassinari@gmail.com>
2013-10-23 19:16:54 +0200

    image url serializer

--- app/serializers/libro_serializer.rb
+++ app/serializers/libro_serializer.rb
@@ -1,6 +1,6 @@
 class LibroSerializer < ActiveModel::Serializer
 
 	attributes :id, :autore, :titolo, :sigla, :prezzo_copertina, :prezzo_consigliato, 
-						 :coefficente, :cm, :ean, :settore, :materia_id, :image
+						 :coefficente, :cm, :ean, :settore, :materia_id, :image_url
 
 end
