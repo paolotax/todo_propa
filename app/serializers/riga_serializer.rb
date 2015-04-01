a21fd6c460f29b651213e84fe0effeb63ba4e6fa
Paolo Tassinari <paolo.tassinari@gmail.com>
2015-04-01 21:13:26 +0200

    appunti v1 controller

--- app/serializers/riga_serializer.rb
+++ app/serializers/riga_serializer.rb
@@ -3,15 +3,14 @@ class RigaSerializer < ActiveModel::Serializer
   attributes :id,
              :uuid, 
       		   :appunto_id, 
-             :libro_id, 
-      			 :titolo, 
+             :libro_id,
     		  	 :quantita, 
       			 :prezzo_unitario, 
       			 :prezzo_copertina, 
       			 :prezzo_consigliato,  
       			 :sconto, 
              :importo,
-      			 :fattura_id,
+      			 :documento_id,
              :state
 
   has_one :libro, serializer: LibroShortSerializer
