74241bc16d45963f76d4206f89cbd57650c1db6a
Paolo Tassinari <paolo.tassinari@gmail.com>
2015-08-28 08:46:15 +0200

    new server 14.04

--- config/recipes/nodejs.rb
+++ config/recipes/nodejs.rb
@@ -1,7 +1,8 @@
 namespace :nodejs do
   desc "Install the latest relase of Node.js"
   task :install, roles: :app do
-    run "#{sudo} add-apt-repository ppa:chris-lea/node.js"
+    # ubuntu 10.04
+    #run "#{sudo} add-apt-repository ppa:chris-lea/node.js"
     run "#{sudo} apt-get -y update"
     run "#{sudo} apt-get -y install nodejs"
   end
