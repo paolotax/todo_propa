0494202db0231160607e754bda271c9c179724ee
Paolo Tassinari <paolo.tassinari@gmail.com>
2015-08-28 17:24:03 +0200

    new server 14.04

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,12 +6,12 @@ require "whenever/capistrano"
 load "config/recipes/base"
 load "config/recipes/nginx"
 load "config/recipes/unicorn"
-load "config/recipes/postgresql"
+#load "config/recipes/postgresql"
 load "config/recipes/nodejs"
 load "config/recipes/rbenv"
-load "config/recipes/memcached"
 load "config/recipes/check"
 load "config/recipes/todopropa"
+load "config/recipes/memcached"
 
 server "178.62.224.167", :web, :app, :db, primary: true
 
